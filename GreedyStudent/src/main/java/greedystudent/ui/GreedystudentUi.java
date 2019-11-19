f6d386ce4a609dee7bfa3f19c149d4a24ab70bc1
Mikxdi <mikko.lehtinen1996@gmail.com>
2019-11-19 11:52:19 +0200

    Luotu ohjelman käynnistävä luokka

--- GreedyStudent/src/main/java/greedystudent/ui/GreedystudentUi.java
+++ GreedyStudent/src/main/java/greedystudent/ui/GreedystudentUi.java
@@ -4,11 +4,30 @@
  * and open the template in the editor.
  */
 package greedystudent.ui;
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
+        
 /**
  *
  * @author lehtmikk
  */
-public class GreedystudentUi {
+public class GreedystudentUi extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
     
+    @Override
+    public void start(Stage stage) throws Exception {
+        GridPane e = new GridPane();
+        Button start = new Button("start");
+        e.add(start, 1, 1);
+        Scene alku = new Scene(e);
+        stage.setScene(alku);
+        stage.show();
+    }
 }
